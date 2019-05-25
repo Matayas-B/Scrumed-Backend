8de83b0e4ac150d0fc74737f1e7f93dc53e32530
Bequita <hernan.beca@gmail.com>
2019-05-25 15:59:48 -0300

    Code - Added methods for creating new scrum and get it.

--- routes/scrum.js
+++ routes/scrum.js
@@ -1,14 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-var scrum = {
-    totalTime: new Date(2019, 1, 15, 0, 11, 0),
-    minutesPerGuest: 1
-}
+var scrumMeetings = []
 
-router.get('/', function (req, res, next) {
+router.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.send(JSON.stringify(scrum));
+});
+
+router.post('/create-scrum', function (request, response) {
+  var scrum = request.body;
+
+  var currentScrum = {
+    id: scrumMeetings.length,
+    meetingTitle: scrum.meetingTitle,
+    totalTime: new Date(2019, 1, 15, 0, 11, 0),
+    minutesPerGuest: scrum.minutesPerGuest,
+    guests: scrum.guests
+  }
+  scrumMeetings.push(currentScrum);
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE');
+  response.send(JSON.stringify(currentScrum.id));
+});
+
+router.get('/get-scrum', function (request, response) {
+  var query = request.query;
+
+  var currentScrum = scrumMeetings.find(scr => scr.id === parseInt(query.id));
+
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'POST,GET,PUT,DELETE');
+  response.send(JSON.stringify(currentScrum));
 });
 
 module.exports = router;
