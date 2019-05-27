bc371299385ec573add8ff69fa913a86f28a2347
Bequita <hernan.beca@gmail.com>
2019-05-27 09:54:39 -0300

    Code - Added delete methods for scrum service.

--- routes/scrum.js
+++ routes/scrum.js
@@ -1,22 +1,53 @@
 var express = require('express');
 var router = express.Router();
 
-var scrumMeetings = []
+var scrumMeetings = [
+  {
+    "id": 1,
+    "meetingTitle": "test",
+    "totalTime": new Date(2019, 1, 15, 0, 11, 0),
+    "minutesPerGuest": 10,
+    "guests": []
+  }
+]
 
 router.get('/', function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.send(JSON.stringify(scrum));
+  res.send(JSON.stringify(scrumMeetings));
+});
+
+router.get('/delete-scrums', function (req, res) {
+  scrumMeetings = []
+
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.sendStatus(200);
+});
+
+router.get('/delete-scrum', function (req, res) {
+  var query = req.query;
+  
+  var indexToRemove = scrumMeetings.findIndex(scr => scr.id === parseInt(query.id))
+
+  if(indexToRemove !== -1) {
+    scrumMeetings.splice(indexToRemove)
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.sendStatus(200);
+  }
+  else {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.send("The scrum does not exist.");
+  }
 });
 
 router.post('/create-scrum', function (request, response) {
   var scrum = request.body;
 
   var currentScrum = {
-    id: scrumMeetings.length,
-    meetingTitle: scrum.meetingTitle,
-    totalTime: new Date(2019, 1, 15, 0, 11, 0),
-    minutesPerGuest: scrum.minutesPerGuest,
-    guests: scrum.guests
+    "id": scrumMeetings.length,
+    "meetingTitle": scrum.meetingTitle,
+    "totalTime": new Date(2019, 1, 15, 0, 11, 0),
+    "minutesPerGuest": scrum.minutesPerGuest,
+    "guests": scrum.guests
   }
   scrumMeetings.push(currentScrum);
 
